61d0d3eb32cda0604a8eb68a3bb7be3c29a744d4
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-05-19 16:56:00 +0530

    Work on Read Functionality

--- src/app/shared/components/todo-dashboard/todo-dashboard.component.ts
+++ src/app/shared/components/todo-dashboard/todo-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Itodos } from '../../materials/todos';
+import { todosData } from '../../consts/todos';
 
 @Component({
   selector: 'app-todo-dashboard',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-dashboard.component.scss']
 })
 export class TodoDashboardComponent implements OnInit {
+  todosArr: Itodos[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todosArr = todosData;
   }
 
 }
