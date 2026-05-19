e39180d1d12207dae500c896ba4f708bb1f0027e
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-05-19 19:27:34 +0530

    Updated conflicts

--- src/app/shared/components/todo-list/todo-list.component.ts
+++ src/app/shared/components/todo-list/todo-list.component.ts
@@ -20,13 +20,11 @@ export class TodoListComponent implements OnInit {
     return todo.todoID;
   }
 
-<<<<<<< HEAD
   onRemove(id: string){
     this.emitRemoveID.emit(id);
-=======
+  }
   onEdit(todo:Itodos){
      this.emitEditObj.emit(todo)
->>>>>>> f435bcf8d85707fbe93ae44cd3d91709cafbf11c
   }
 
 }
